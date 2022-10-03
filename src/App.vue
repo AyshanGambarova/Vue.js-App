db5ac4f66f2a3e101d096d29fb4d667f7c692eed
AyshanGambarova <ayshanag@code.edu.az>
2022-10-03 18:40:29 +0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Create contact</h1>
+    <div id="form">
+      <div>
+        <label>Name and Surname:</label>
+        <input type="text" v-model="name" />
+      </div>
+      <br />
+      <div>
+        <label>Phone:</label>
+        <input type="text" v-model="phone" />
+      </div>
+      <br />
+      <div>
+        <button @click="handleSubmit">Save</button>
+      </div>
+    </div>
+    <br />
+    <br />
+    <ContactList :data="contacts" @delete="deleteContact"></ContactList>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ContactList from "./components/ContactList.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      name: "",
+      phone: "",
+      contacts: [],
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.contacts.push({ name: this.name, phone: this.phone });
+      this.name = "";
+      this.phone = "";
+    },
+    deleteContact(index) {
+      this.contacts.splice(index, 1);
+    },
+  },
+  components: { ContactList },
+};
 </script>
 
 <style>
---
+++ src/components/ContactList.vue
@@ -0,0 +1,22 @@
+<template>
+   <div>
+    <div v-for="(contact,index) in data" :key="index">
+   <div> {{contact.name}}-{{contact.phone}}-#{{index}} <button @click="removeContact(index)">Remove</button></div>
+    </div>
+   </div>
+</template>
+<script>
+export default {
+    props:['data'],
+    name: "ContactList",
+    methods:{
+        removeContact(){
+            this.$emit('delete');
+        }
+    }
+}
+
+</script>
+<style>
+
+</style>
